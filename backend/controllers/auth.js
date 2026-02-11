4d16515c154b88ad2a2c3386c578bef694a09b18
deep_saini1912 <deepsaini1912@gmail.com>
2026-02-12 02:12:25 +0530

    Implement Login Route + JWT Authentication

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,8 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-module.exports.registerUser = async (req, res) => {
+module.exports.registerUser = async(req, res) => {
 
     const { name, email, password, avatar } = req.body;
 
@@ -31,5 +32,41 @@ module.exports.registerUser = async (req, res) => {
     } catch (error) {
         console.error("Server Problem", error);
         res.status(500).json({ success: false, message: "Server Problem", error: error });
+    }
+}
+
+module.exports.loginUser = async(req,res) => {
+    const {email,password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({success:false,message:"Please provide all credentials to get logged!"});
+    }
+
+    try {
+        const user = await User.findOne({email}).select("+password");
+
+        if(!user){
+            return res.status(400).json({success:false,message:"Invalid Credentials"});
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+            return res.status(400).json({success:false,message:"Invalid Credentials"});
+        }
+
+        const payload = {
+            id:user._id
+        };
+
+        const token = jwt.sign(payload,process.env.JWT_SECRET,{
+            expiresIn:"7d"
+        });
+
+        res.status(200).json({success:true,message:"Login Successful!",token:token,data:{name:user.name,email:user.email,id:user._id}});
+
+    } catch (error) {
+        console.error("Server Problem ",error);
+        res.status(500).json({success:false,message:"Server Problem",error:error});
     }
 }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,4 +5,6 @@ const authController = require("../controllers/auth");
 
 router.post('/register',authController.registerUser);
 
+router.post('/login',authController.loginUser);
+
 module.exports = router;
